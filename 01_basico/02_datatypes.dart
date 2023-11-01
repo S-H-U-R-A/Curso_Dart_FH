ac0c67d92518b4046cf3a879147d964462ca8800
SergioPcWindows <sandres0190@gmail.com>
2023-11-01 16:16:55 -0500

    feat(App): Inicio del curso

---
+++ 01_basico/02_datatypes.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+main() {
+  // ===== Números
+  int a = 10;
+  double b = 5.5;
+
+  int? c;
+
+  print(c);
+
+  // ==== Strings
+  String nombre = 'Tony';
+
+  print(nombre);
+}
